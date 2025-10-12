053394342e937199835565facdf6fa133f683b15
sonjinile <sy7521012@naver.com>
2025-10-12 10:36:25 +0900

    상품현황 선택삭제 구현 및 그와 연관된 데이터들 DELETE CASCADE 적용하여 연관된 데이터들 모두 삭제되도록 구현

--- src/main/java/kr/co/bnk_marketproject_be/controller/AdminProductController.java
+++ src/main/java/kr/co/bnk_marketproject_be/controller/AdminProductController.java
@@ -8,6 +8,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -38,5 +43,14 @@ public class AdminProductController {
         model.addAttribute("pageResponseDTO", pageResponseDTO);
 
         return "admin/admin_product_searchList";
+    }
+
+    @PostMapping("/admin/product/delete")
+    @ResponseBody
+    public String deleteProducts(@RequestBody List<Integer> ids) {
+        log.info("ids:{}",ids);
+
+        adminMemberService.deleteProducts(ids);
+        return "OK";
     }
 }
