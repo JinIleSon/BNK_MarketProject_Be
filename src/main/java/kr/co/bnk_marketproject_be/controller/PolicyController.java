1e7f015b5b6a3d6849da1010698879570ac81801
thswlsdlf0000 <sy7521012@naver.com>
2025-10-14 10:26:43 +0900

    정책 부분 DB 연동 및 데이터 구현 완료

--- src/main/java/kr/co/bnk_marketproject_be/controller/PolicyController.java
+++ src/main/java/kr/co/bnk_marketproject_be/controller/PolicyController.java
@@ -1,17 +1,22 @@
 package kr.co.bnk_marketproject_be.controller;
 
+import kr.co.bnk_marketproject_be.service.PolicyService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @Slf4j
 @RequiredArgsConstructor
-public class PrivacyController {
+public class PolicyController {
+
+    private final PolicyService policyService;
 
     @GetMapping("/policy/policy/terms")
-    public String list(){
+    public String list(Model model) {
+        model.addAttribute("dtoList", policyService.selectAllPolicies());
         return "policy/policy_terms";
     }
 }
