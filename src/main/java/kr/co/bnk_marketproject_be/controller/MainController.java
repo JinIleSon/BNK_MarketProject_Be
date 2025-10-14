7aa5de28bc18f679266ac93568cebf5558058875
thswlsdlf0000 <sy7521012@naver.com>
2025-10-14 12:20:39 +0900

    메인 내용물 완료, 헤더, 푸터 진행중

--- src/main/java/kr/co/bnk_marketproject_be/controller/MainController.java
+++ src/main/java/kr/co/bnk_marketproject_be/controller/MainController.java
@@ -1,8 +1,10 @@
 package kr.co.bnk_marketproject_be.controller;
 
+import kr.co.bnk_marketproject_be.service.MainService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -10,8 +12,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequiredArgsConstructor
 public class MainController {
 //main 푸시
+
+    private final MainService mainService;
+
     @GetMapping("/main/main/page")
-    public String list(){
+    public String list(Model model){
+        model.addAttribute("newList",       mainService.getProducts(null,        8)); // created_at desc
+        model.addAttribute("bestList",      mainService.getProducts("sold",      5)); // 판매수 상위 5
+        model.addAttribute("hitList",       mainService.getProducts("hits",      8)); // 조회수 상위 8
+        model.addAttribute("recoList",      mainService.getProducts("rating",    8)); // 평점 상위 8
+        model.addAttribute("discountList",  mainService.getProducts("discount",  8)); // 할인율 상위 8
         return "main/main_main";
     }
 }
