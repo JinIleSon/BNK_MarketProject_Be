6a17f53f9ce852f8b81732469d2fdb05f9c41a30
thswlsdlf0000 <sy7521012@naver.com>
2025-10-17 12:02:48 +0900

    cs main notice 완료

--- src/main/java/kr/co/bnk_marketproject_be/controller/CSMainController.java
+++ src/main/java/kr/co/bnk_marketproject_be/controller/CSMainController.java
@@ -1,8 +1,12 @@
 package kr.co.bnk_marketproject_be.controller;
 
+import kr.co.bnk_marketproject_be.dto.PageRequestDTO;
+import kr.co.bnk_marketproject_be.dto.PageResponseAdminAnnouncementDTO;
+import kr.co.bnk_marketproject_be.service.AdminAnnouncementService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -10,8 +14,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequiredArgsConstructor
 public class CSMainController {
 
+    private final AdminAnnouncementService adminAnnouncementService;
+
     @GetMapping("/cs/index")
-    public String index() {
+    public String index(Model model, PageRequestDTO pageRequestDTO) {
+
+        PageResponseAdminAnnouncementDTO pageResponseAdminAnnouncementDTO = adminAnnouncementService.selectAllAdminAnnouncement(pageRequestDTO);
+
+        log.info("pageResponseAdminAnnouncementDTO={}", pageResponseAdminAnnouncementDTO);
+        model.addAttribute("pageResponseAdminAnnouncementDTO", pageResponseAdminAnnouncementDTO);
+
         return "customer_service/cs_main";
     }
+
+
 }
